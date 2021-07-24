24542de585d342beb8a118215bb29e581dc9ff96
mohit kushwaha <kushwahamohit000@gmail.com>
2021-07-24 14:38:45 +0530

    final setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: [],
+  // purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
       poppins: ["Poppins"],
       // noto: ["Noto Sans"],
     },
-    extend: {},
+    extend: {
+      zIndex: {
+        minus1: "-1",
+      },
+    },
+    backgroundColor: (theme) => ({
+      ...theme("colors"),
+      bggrey: "#f9fafc",
+    }),
   },
   variants: {
     extend: {},
