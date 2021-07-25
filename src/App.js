7b77176c67347f1fde28dc0ebdd4684c250dce5f
mohit kushwaha <kushwahamohit000@gmail.com>
2021-07-25 18:28:38 +0530

    feat: signup route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Login } from "../src/Pages/index";
+import { Login, Signup } from "../src/Pages/index";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
   );
