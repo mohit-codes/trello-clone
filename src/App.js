8efbd1b0fc2851403f06fc993b6068436da089a0
mohit kushwaha <kushwahamohit000@gmail.com>
2021-07-28 18:18:19 +0530

    feat: private route | home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Login, Signup } from "../src/Pages/index";
+import { Header, PrivateRoute } from "./Components/index";
+import { Home } from "./Pages/Home/Home";
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Login />} />
+        <div className="h-full flex flex-col">
+          <Header />
+          <Routes>
+            <PrivateRoute path="/" element={<Home />} />
+          </Routes>
+        </div>
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
