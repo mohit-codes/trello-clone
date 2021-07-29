fd826b21032a3df3475fdcd01c9aa283ccd8ca8f
mohit kushwaha <kushwahamohit000@gmail.com>
2021-07-29 18:34:49 +0530

    feat: cusotm hook

---
+++ src/hooks/useAxiosGet.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-unused-vars */
+
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { backendUrl } from "../util/constant";
+
+export const useAxiosGet = (path, id) => {
+  const [data, setData] = useState();
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState();
+  useEffect(() => {
+    let unmounted = false;
+    const fetch = async () => {
+      try {
+        const res = axios.get(backendUrl + path + id);
+        setData(res.body);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+        setError(true);
+        setErrorMessage(err.message);
+      }
+    };
+    fetch();
+    return () => {
+      unmounted = true;
+    };
+  }, [path]);
+
+  const addItem = (item) => {
+    setData((prevData) => [...prevData, item]);
+  };
+
+  const removeItem = (id) => {
+    setData((prevData) => prevData.filter((obj) => obj._id !== id));
+  };
+
+  return {
+    data,
+    error,
+    setData,
+    loading,
+    errorMessage,
+    addItem,
+    removeItem,
+  };
+};
