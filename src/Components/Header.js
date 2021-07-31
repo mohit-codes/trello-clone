5d800cf35b4fa78d6c01bd90fc828e5d4b2ff896
mohit kushwaha <kushwahamohit000@gmail.com>
2021-07-31 12:09:08 +0530

    feat: blur div added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../Context/AuthProvider";
 import logo from "../assets/logo2.png";
@@ -7,8 +8,8 @@ export const Header = () => {
   const { user, logout } = useAuth();
   return (
     <>
-      <nav className="flex z-10 h-16 justify-between items-center px-10">
-        <div className="flex h-10 items-center">
+      <nav className="pl-4 md:p-0 flex z-10 h-16 justify-between items-center">
+        <div className="hidden md:flex h-10 items-center ml-10">
           <i className="fa fa-search text-gray-400"></i>
           <input
             className=" border-none outline-none px-2 py-1 "
@@ -16,15 +17,28 @@ export const Header = () => {
             placeholder="Search Boards"
           />
         </div>
-        <div className="cursor-pointer" onClick={useNavigate("/")}>
+        <div
+          className="cursor-pointer"
+          onClick={() => {
+            useNavigate("/");
+          }}
+        >
           <img className="h-10 " src={logo} />
         </div>
-        <div className="space-x-10">
+        <div className="space-x-10 mr-10">
           <span> Hello, {user?.username} </span>
-          <span onClick={() => logout()}>
-            <i className="fa fa-sign-out text-red-600 cursor-pointer text-xl"></i>
+          <span
+            onClick={() => {
+              logout();
+            }}
+          >
+            <i
+              title="Logout"
+              className="fa fa-sign-out text-red-600 cursor-pointer text-xl"
+            ></i>
           </span>
         </div>
+        <div className="out-of-focus"></div>
       </nav>
     </>
   );
