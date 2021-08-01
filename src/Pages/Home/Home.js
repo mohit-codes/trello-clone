c1d71262ed9fd4a56e86fa489853e21e9de75948
mohit kushwaha <kushwahamohit000@gmail.com>
2021-08-01 17:06:32 +0530

    feat: App's home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,54 +1,76 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 import CreateProjectModal from "../../Components/modals/createProjectModal";
 import { useAxiosGet } from "../../hooks/useAxiosGet";
-import { useAuth } from "../../Context/AuthProvider";
+import CreateBoard from "../../Components/modals/createboard";
 export const Home = () => {
   const [showAddProjectModal, setshowAddProjectModal] = useState(false);
-  const { user } = useAuth();
+  const [showAddBoardModal, setshowAddBoardModal] = useState(false);
 
-  const { addItem: addProject, data: projects } = useAxiosGet(
-    "projects",
-    user._id
-  );
+  const { addItem: addProject, data: projects } = useAxiosGet("projects");
+  const { addItem: addBoard, data: boards } = useAxiosGet("boards");
   return (
     <>
       <div className="flex-1 min-w-full bg-bggray">
-        <section className="flex space-x-10  md:mx-20 lg:mx-48 py-4">
-          <div className="hidden md:block space-y-3">
-            <ul>
-              <li className="text-sm">Boards</li>
-            </ul>
-            <div className="flex items-center space-x-8">
-              <p className="text-lg text-black font-medium">Projects</p>
-              <i
+        <section className="space-y-5 px-10 py-5">
+          <div className="">
+            <div>
+              <i className="fa fa-user-o text-lg mr-3" />
+              <span className="text-xl text-black font-medium">
+                Personal Boards
+              </span>
+            </div>
+            <div className="flex flex-wrap">
+              {boards?.map((board, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="p-2 w-60 h-32 mr-5 mt-6 border-2 border-black  hover:shadow-lg cursor-pointer"
+                  >
+                    {board.title}
+                  </div>
+                );
+              })}
+              {showAddBoardModal ? (
+                <CreateBoard
+                  setShowModal={setshowAddBoardModal}
+                  addBoard={addBoard}
+                />
+              ) : (
+                <div
+                  onClick={() => setshowAddBoardModal(true)}
+                  className="w-60 h-32 text-center py-12 mt-6 bg-gray-200 text-gray-500 cursor-pointer"
+                >
+                  Create new board...
+                </div>
+              )}
+            </div>
+          </div>
+          <div className="">
+            <div>
+              <i className="fa fa-users fa-outlined text-lg mr-3" />
+              <span className="text-xl text-black font-medium">
+                Teams/Projects
+              </span>
+            </div>
+            <div className="flex flex-wrap">
+              {projects?.map((project, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="p-2 w-60 h-32 mr-5 mt-6 border-2 border-black  hover:shadow-lg cursor-pointer"
+                  >
+                    {project.title}
+                  </div>
+                );
+              })}
+              <div
                 onClick={() => {
                   setshowAddProjectModal(true);
                 }}
-                className="fa fa-plus text-sm font-thin cursor-pointer"
-              />
-            </div>
-            <ul className="space-y-1">
-              {projects?.map((project, index) => {
-                return (
-                  <li key={index} className="text-sm">
-                    <i className="fa fa-users text-sm fa-outlined px-1" />
-                    <span>{project.title}</span>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-          <div>
-            <div className="flex  justify-between lg:space-x-96">
-              <div>
-                <i className="fa fa-user-o text-lg px-3" />
-                <span>Personal Boards</span>
+                className="w-60 h-32 text-center py-12 mt-6 bg-gray-200 text-gray-500 cursor-pointer"
+              >
+                Create new project...
               </div>
-              <button className="bg-blue-500 text-white px-5 py-1 rounded-md space-x-2">
-                <i className="fa fa-plus text-sm font-thin" />
-                <span>Create</span>
-              </button>
             </div>
           </div>
         </section>
