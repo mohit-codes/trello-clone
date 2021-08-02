f8dadd2ce179cb41ad76256e83ece84643830b5d
mohit kushwaha <kushwahamohit000@gmail.com>
2021-08-02 18:31:24 +0530

    feat: exports

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,2 +1,6 @@
 export { Header } from "./Header";
 export { PrivateRoute } from "./PrivateRoute";
+export { CreateBoard } from "./createboard";
+export { CreateProjectModal } from "./modals/createProjectModal";
+export { CreateList } from "./createList";
+export { List } from "./List";
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,2 +1,4 @@
 export { Login } from "./Login/Login.js";
 export { Signup } from "./Signup/Signup.js";
+export { Home } from "./Home/Home";
+export { Board } from "./Board/Board";
