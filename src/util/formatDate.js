99d633c1e29c66412a320fd8bb50eaa4c50c3b67
mohit kushwaha <kushwahamohit000@gmail.com>
2021-08-06 18:35:59 +0530

    feat: exports

--- src/util/formatDate.js
+++ src/util/formatDate.js
@@ -1,5 +1,6 @@
 // yyyy-mm-dd to dd-mm-yyyy
-export default function formatDate(date) {
+function formatDate(date) {
   const [yyyy, mm, dd] = date.substr(0, 10).split("-");
   return `${dd}-${mm}-${yyyy}`;
 }
+export { formatDate };
