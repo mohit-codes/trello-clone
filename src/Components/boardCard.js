5b42e11e3cbe5766b20aabeef541a754e7759e27
mohit kushwaha <kushwahamohit000@gmail.com>
2021-08-10 16:24:02 +0530

    fix: text white

--- src/Components/boardCard.js
+++ src/Components/boardCard.js
@@ -5,7 +5,7 @@ export const BoardCard = ({ object, to }) => {
   return (
     <div>
       <Link className="" to={`/${to}/${object._id}`} state={{ object }}>
-        <div className="p-2 w-60 h-32 mr-5 mt-6 border-2 border-black bg-blue-500  hover:shadow-lg cursor-pointer">
+        <div className="p-2 w-60 h-32 mr-5 mt-6 border-2 border-black bg-blue-500 text-white  hover:shadow-lg cursor-pointer">
           {object.title}
         </div>
       </Link>
