cd8c264ed190aad4aaa0fe18b4e9455979c7202b
mohit kushwaha <kushwahamohit000@gmail.com>
2021-08-11 18:59:11 +0530

    adminId prop

--- src/Components/boardCard.js
+++ src/Components/boardCard.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-export const BoardCard = ({ object, to }) => {
+export const BoardCard = ({ object, to, projectAdmin }) => {
   return (
     <div>
-      <Link className="" to={`/${to}/${object._id}`} state={{ object }}>
+      <Link
+        className=""
+        to={`/${to}/${object._id}`}
+        state={{ object, projectAdmin }}
+      >
         <div className="p-2 w-60 h-32 mr-5 mt-6 border-2 border-black bg-blue-500 text-white  hover:shadow-lg cursor-pointer">
           {object.title}
         </div>
@@ -15,4 +19,5 @@ export const BoardCard = ({ object, to }) => {
 BoardCard.propTypes = {
   object: PropTypes.object,
   to: PropTypes.string,
+  projectAdmin: PropTypes.string,
 };
