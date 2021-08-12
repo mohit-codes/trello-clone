93c42f0f49b17a5fc2c8ec6b505375ee2ecd31ba
mohit kushwaha <kushwahamohit000@gmail.com>
2021-08-12 19:10:31 +0530

    feat: custom spinner |  admin checks

--- src/Components/List.js
+++ src/Components/List.js
@@ -5,17 +5,22 @@ import { CreateCard } from "./index";
 import { axiosDelete, backendUrl } from "../util/index";
 import axios from "axios";
 import { ListCard } from "./listCard";
+import { useAuth } from "../Context/AuthProvider";
+import { Loading } from "./loading";
 
 export const List = ({ list, removeList, projectAdmin }) => {
   const [listState, setListState] = useState(list);
   const [showListEditOptions, setshowListEditOptions] = useState("");
   const [showCreateCard, setShowCreateCard] = useState(false);
-
+  const { user } = useAuth();
+  const isAdmin = projectAdmin === undefined ? true : projectAdmin === user._id;
   const [newTitle, setNewTitle] = useState(list.title);
+
   const {
     data: cards,
     addItem: addCard,
     removeItem: removeCard,
+    loading: loadingCards,
   } = useAxiosGet("lists/cards", listState._id);
   const editHandler = async (e) => {
     e.preventDefault();
@@ -31,24 +36,34 @@ export const List = ({ list, removeList, projectAdmin }) => {
   };
 
   return (
-    <div className=" p-2 w-64 h-auto flex-shrink-0 space-y-3 bg-blue-400 rounded-md border-2 border-black flex flex-col">
+    <div className="mr-3 p-2 w-64 h-auto flex-shrink-0 space-y-3 bg-blue-400 rounded-md border-2 border-black flex flex-col">
       <div className="flex items-center justify-between ">
         <p>
-          <input
-            type="text"
-            className="px-1 border-none outline-none w-40 font-medium focus:font-thin bg-transparent focus:bg-white focus:text-black "
-            value={newTitle}
-            onChange={({ target }) => setNewTitle(target.value)}
-            onBlur={(event) => editHandler(event)}
-          />
+          {isAdmin ? (
+            <input
+              type="text"
+              className="px-1 border-none outline-none w-40 font-medium focus:font-thin bg-transparent focus:bg-white focus:text-black "
+              value={newTitle}
+              onChange={({ target }) => setNewTitle(target.value)}
+              onBlur={(event) => editHandler(event)}
+            />
+          ) : (
+            <span>{newTitle}</span>
+          )}
         </p>
         <div>
           {showListEditOptions && (
             <>
               <i
+                className="fa fa-info-circle"
+                title="Board owner can edit list title by clicking on title"
+              ></i>
+              <i
                 title="delete"
-                className="fa fa-trash  text-xl ml-2 cursor-pointer"
-                onClick={(event) => deleteHandler(event)}
+                className={`fa fa-trash  text-xl ml-2 ${
+                  isAdmin ? "cursor-pointer" : "cursor-not-allowed"
+                }`}
+                onClick={isAdmin ? (event) => deleteHandler(event) : null}
               ></i>
             </>
           )}
@@ -61,17 +76,21 @@ export const List = ({ list, removeList, projectAdmin }) => {
           />
         </div>
       </div>
-      {cards?.map((card) => {
-        return (
-          <ListCard
-            key={card._id}
-            list={listState}
-            removeCard={removeCard}
-            card={card}
-            projectAdmin={projectAdmin}
-          />
-        );
-      })}
+      {loadingCards ? (
+        <Loading />
+      ) : (
+        cards?.map((card) => {
+          return (
+            <ListCard
+              key={card._id}
+              list={listState}
+              removeCard={removeCard}
+              card={card}
+              projectAdmin={projectAdmin}
+            />
+          );
+        })
+      )}
       {showCreateCard && (
         <CreateCard
           listId={listState._id}
