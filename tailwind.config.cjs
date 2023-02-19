781b5e7a23009b8702c87348fb46f51d136306c8
mohit-codes <kushwahamohit000@gmail.com>
2023-02-20 00:34:43 +0530

    chore: upgrade packages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,5 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false,
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     fontFamily: {
       poppins: ["Poppins"],
